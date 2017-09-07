91e6943824c6a3694ca093f8cf5457f09541e3d0
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-09-07 12:29:07 -0600

    Created the main and start method.

--- src/process/controller/ProcessController.java
+++ src/process/controller/ProcessController.java
@@ -5,6 +5,7 @@ public class ProcessController
 
 	public void start()
 	{
-		
+		System.out.println("This is a practice project.");
+		System.out.println("By practicing I develop muscle memory :D");
 	}
 }
